c5d5d9c3072af50801074acb839edd01ef94ed10
VitaliiZhu <23zhuk.v@nltu.lviv.ua>
2025-09-19 10:36:28 +0300

    try3

--- lab2.playground/Contents.swift
+++ lab2.playground/Contents.swift
@@ -7,13 +7,15 @@ enum ApartmentType: String {
 }
 
 struct Apartment {
-    var rooms:Int = 1
-    var floor:Int? = nil
-    var balcony:Bool? = nil
-    var ap_type:ApartmentType
+    var rooms: Int = 1
+    var floor: Int? = nil
+    var balcony: Bool? = nil
+    var area: Int
+    var ap_type: ApartmentType
     
-    func Describe() -> String {
+    func describe() -> String {
         var result = "Type : \(ap_type) , Rooms : \(rooms) ,"
+        
         if let apartrmentFlor = floor {
             result = result + " Floor : \(apartrmentFlor) ,"
         } else {
@@ -24,16 +26,18 @@ struct Apartment {
         } else  {
             result = result + " Balcony : Unknown "
         }
+        
         return result
     }
 }
+
 struct Owner {
     var name: String
-    var apartment:Apartment? = nil
-    func Describe() -> String {
+    var apartment: Apartment? = nil
+    func describe() -> String {
         var info = "Name : \(name) , "
         if let unwrappedApartment = apartment {
-             info = info + String(" Apartment : \(unwrappedApartment.Describe())")
+             info = info + String(" Apartment : \(unwrappedApartment.describe())")
         } else {
              info = info + " Apartment : no info"
         }
@@ -42,34 +46,35 @@ struct Owner {
 }
 
 func describeApartment(apartment:Apartment) -> String {
-    var result = "Type : \(apartment.ap_type) , Rooms : \(apartment.rooms) ,"
-    if let apartrmentFlor = apartment.floor {
-        result = result + " Floor : \(apartrmentFlor) ,"
-    } else {
-        result = result + " Floor : Unknown , "
-    }
-    if let apartmentBalcony = apartment.balcony {
-        result = result + " Balcony : \(apartmentBalcony) "
-    } else  {
-        result = result + " Balcony : Unknown "
-    }
-    return result
+    return apartment.describe()
 }
+func calculateArea(apartments: [Apartment], type: ApartmentType) -> Int {
+    var area = 0
+   
+    for ap in apartments {
+        if type == ap.ap_type{
+            area = area + ap.area
+        }
+    }
+    return area
+}
+var apartments: [Apartment] = [Apartment(rooms: 1, floor: 1, balcony: true, area: 10, ap_type: .studio)]
 
-var apartments: [Apartment] = [Apartment(rooms: 1, floor: 1, balcony: true, ap_type: .studio)]
-let newApartment = Apartment(rooms: 2, floor: 1, balcony: false, ap_type: .twoBedroom)
+let newApartment = Apartment(rooms: 2, floor: 1, balcony: false, area: 10, ap_type: .twoBedroom)
 apartments.append(newApartment)
-let newApartment1 = Apartment(rooms: 1, balcony: false, ap_type: .oneBedroom)
+let newApartment1 = Apartment(rooms: 1, balcony: false, area: 10, ap_type: .oneBedroom)
 apartments.append(newApartment1)
-let newApartment2 = Apartment(rooms: 2, floor: 1, ap_type: .twoBedroom)
+let newApartment2 = Apartment(rooms: 2, floor: 1, area: 10, ap_type: .twoBedroom)
 apartments.append(newApartment2)
-let newApartment3 = Apartment(rooms: 1, ap_type: .studio)
+let newApartment3 = Apartment(rooms: 1, area: 10, ap_type: .studio)
 apartments.append(newApartment3)
 for apartment in apartments {
-    print(apartment.Describe())
+    print(apartment.describe())
 }
+
+print(String("Area of twoBedrooms = \(calculateArea(apartments: apartments, type: .twoBedroom))"))
 print(describeApartment(apartment: newApartment))
 let mykola = Owner(name: "Mykola", apartment: apartments[1])
-print(mykola.Describe())
+print(mykola.describe())
 let dimon = Owner(name: "Dmytro")
-print(dimon.Describe())
+print(dimon.describe())
